ca8041d112e4f045efe981767efa8baa737451b4
German <gnshibanov@mail.ru>
2024-12-15 10:21:24 +0300

    added checkbox clicking

--- src/test/java/com/mts/booking_test/slenide/Test1.java
+++ src/test/java/com/mts/booking_test/slenide/Test1.java
@@ -1,7 +1,9 @@
 package com.mts.booking_test.slenide;
 
 
+import com.codeborne.selenide.Selenide;
 import com.mts.booking_test.pages.MainPageSelenide;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,13 +22,12 @@ public class Test1 {
 
     @Test
     public void test1() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "chromedriver131.exe");
         open("https://www.booking.com/searchresults.ru.html?ss=Анталья&ssne=Анталья&ssne_untouched=Анталья&efdco=1&label=gen173nr-1BCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEV2AEB6AEBiAIBqAIDuAK42uu6BsACAdICJGVkYmUyNjJhLTA2MjUtNGYyMi04NjU5LWY4OTg5OGQ5NTIwZdgCBeACAQ&sid=2c43ea7c87750b4fe6a929eb2e151c44&aid=304142&lang=ru&sb=1&src_elem=sb&src=searchresults&dest_id=-735347&dest_type=city&group_adults=2&no_rooms=1&group_children=0");
         sleep(10000);
+        Selenide.$("#\\:r29\\:").click();
+        //Selenide.$("#ss").val("Анталья").pressEnter();
         boolean result = mainPageSelenide.searchForStars();
-        if (result) {
-            System.out.println("Тест пройден!");
-        }
-        else System.out.println("Тест не пройден!");
+        Assertions.assertEquals(true, result, "Тест не пройден!");
     }
 }
